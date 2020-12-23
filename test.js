44d9f38aee22f27d4f7f428de0e5ded65a343cd3
maggot_code <wowhuanhuan233@163.com>
2020-12-23 18:37:03 +0800

    add test

---
+++ test.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: maggot-code
+ * @Date: 2020-12-23 18:33:21
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2020-12-23 18:33:52
+ * @Description: file content
+ */
+exports.printMsg = function () {
+    console.log("Hello Npm");
+}
