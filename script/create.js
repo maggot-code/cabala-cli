374c299d6b8db7deebbb3666f70235547c752b85
maggot_code <wowhuanhuan233@163.com>
2020-12-25 00:21:18 +0800

    模板创建命令测试

--- script/create.js
+++ script/create.js
@@ -2,11 +2,16 @@
  * @Author: maggot-code
  * @Date: 2020-12-24 12:42:16
  * @LastEditors: maggot-code
- * @LastEditTime: 2020-12-24 13:21:02
+ * @LastEditTime: 2020-12-25 00:20:45
  * @Description: file content
  */
+const path = require('path');
+const fs = require('fs');
+
 const templateType = require('../package.json').template;
 const chalk = require('chalk');
+const inquirer = require('inquirer');
+const validateProjectName = require('validate-npm-package-name');
 
 async function create(type, projectName, options) {
     // 检查是否存在该模板
@@ -18,8 +23,56 @@ async function create(type, projectName, options) {
 
     // 获取node进程的工作目录
     const cwd = process.cwd();
-    console.log(cwd);
-    return Promise.resolve('success');
+
+    // 判断是否是当前目录
+    const inCurrentDir = projectName === '.';
+
+    // 获取项目名(当前目录 or 指定的项目名)
+    const name = inCurrentDir ? path.relative('../', cwd) : projectName;
+
+    // 真正的目录地址
+    const targetDir = path.resolve(cwd, projectName);
+
+    // 校验项目名(包名)是否合法
+    const validateResult = validateProjectName(name);
+    if (!validateResult.validForNewPackages) {
+        // 打印出错误以及警告
+        console.error(chalk.red(`Invalid project name: "${name}"`));
+        validateResult.errors &&
+            validateResult.errors.forEach(error => {
+                console.error(chalk.red.dim(`Error: ${error}`));
+            });
+        validateResult.warnings &&
+            validateResult.warnings.forEach(warn => {
+                console.error(chalk.yellow.dim(`Warning: ${warn}`));
+            });
+        process.exit(1);
+    }
+
+    /**
+     * @step1 判断目录是否存在
+     * @step2 如果存在 -> 中止程序，提示用户
+     * @step3 如果不存在 -> 创建目录 -> copy模板到该目录下
+     * @step4 copy模板到该目录下
+     * @step5 安装成功！友好提示
+     */
+    if (fs.existsSync(targetDir)) {
+        return Promise.reject(chalk.red(`${chalk.yellow(cwd)} exist "${name}" dir!`));
+    } else {
+        const { ok } = await inquirer.prompt([
+            {
+                name: "ok",
+                type: "confirm",
+                message: `You confirm dir create ${chalk.green(`${type} template`)} "${name}" ?`
+            }
+        ])
+        if (!ok) {
+            return Promise.reject(chalk.yellow(`You cancel`))
+        } else {
+            console.log('copy to');
+            return Promise.resolve('success');
+        }
+    }
 }
 
 module.exports = (type, projectName, ...args) => {
---
+++ template/copyTemplate.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: maggot-code
+ * @Date: 2020-12-24 23:30:27
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2020-12-24 23:30:27
+ * @Description: file content
+ */
