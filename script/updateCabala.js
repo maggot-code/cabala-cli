731618b31599c8008360adb957eb8ff832749f61
maggot_code <wowhuanhuan233@163.com>
2020-12-27 02:06:52 +0800

    feat：“加入npm包版本更新指令”

--- script/updateCabala.js
+++ script/updateCabala.js
@@ -2,17 +2,18 @@
  * @Author: maggot-code
  * @Date: 2020-12-26 01:37:46
  * @LastEditors: maggot-code
- * @LastEditTime: 2020-12-26 01:39:06
+ * @LastEditTime: 2020-12-27 02:05:29
  * @Description: file content
  */
+const chalk = require('chalk');
 const { shellRun } = require('./utils');
 const updateCabala = () => {
-    console.log('正在更新...');
-    setTimeout(() => {
-        console.log('✨ 更新完成');
-        console.log(shellRun(`cabala -v`));
-        console.log(shellRun(`cabala`));
-    }, 6000);
+    console.log(`✨ update "${chalk.green('@hanserena/cabala-cli')}" ...`);
+    console.log(shellRun('npm update @hanserena/cabala-cli -g'));
+    console.log('✨ update success!');
+    console.log(shellRun(`cabala -v`));
+    console.log(shellRun(`cabala`));
+    process.exit(1);
 }
 module.exports = {
     updateCabala
